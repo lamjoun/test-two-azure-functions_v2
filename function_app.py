5c15e0bc77e8e6b525629987f2f5309881a62ca1
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-11-23 21:03:48 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,5 +1,10 @@
 import azure.functions as func
 import logging
+#from io import StringIO
+import pandas as pd
+import json
+#import os
+import io
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -25,7 +30,34 @@ def pythonfunction_def(req: func.HttpRequest) -> func.HttpResponse:
             status_code=200
         )
 
+# Fonction 1 : Calculer le max
+@app.function_name(name="max_function")
+@app.route(route="max_function", methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)  # Binding HTTP avec méthode POST 
+def max_function_def(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Execution de la fonction Azure: max_function...")
 
+    try:
+        # import
+        #from azure.storage.blob import BlobServiceClient
+        from io import StringIO
+        #import pandas as pd
+        #import json
+        import os
+        #
+        # Vérifiez si les données JSON sont bien reçues
+        req_body = req.get_json()
+        blob_name = req_body.get('file')
+        column = req_body.get('column')
+
+        # Vérifiez la récupération des paramètres
+        logging.info(f"Nom du fichier: {blob_name}")
+        logging.info(f"Nom de la colonne: {column}")
+
+        # Assurez-vous que le fichier et la colonne sont fournis
+        if not blob_name or not column:
+            return func.HttpResponse("Paramètres 'file' ou 'column' manquants.", status_code=400)
+
+# anotherfunction
 @app.function_name("anotherfunction")
 @app.route(route="anotherfunction", methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)  # Added the decorator here
 def anotherfunction(req: func.HttpRequest) -> func.HttpResponse:
