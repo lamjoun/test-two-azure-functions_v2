285e889cfa554d1a72eea5c6b3217e8a1c04c119
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-11-23 21:23:31 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+from azure.storage.blob import BlobServiceClient
 import azure.functions as func
 import logging
 #from io import StringIO
@@ -32,7 +33,7 @@ def pythonfunction_def(req: func.HttpRequest) -> func.HttpResponse:
 
 # Fonction 1 : Calculer le max
 @app.function_name("max_function")
-@app.route(route="max_function", methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)  # Binding HTTP avec méthode POST 
+@app.route(route="max_function", methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)  # Added the decorator here
 def max_function_def(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Execution de la fonction Azure: max_function...")
 
@@ -56,6 +57,34 @@ def max_function_def(req: func.HttpRequest) -> func.HttpResponse:
         # Assurez-vous que le fichier et la colonne sont fournis
         if not blob_name or not column:
             return func.HttpResponse("Paramètres 'file' ou 'column' manquants.", status_code=400)
+
+        # calcul du mean
+        # Connexion au Blob Storage
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+        container_name = "test21container"
+        
+        # Accéder au blob (fichier CSV)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_data = blob_client.download_blob().content_as_text()
+
+        # Charger les données CSV dans un DataFrame pandas
+        df = pd.read_csv(StringIO(blob_data))
+        
+        # Vérifier si la colonne existe dans le fichier
+        if column not in df.columns:
+            return func.HttpResponse("La colonne spécifiée n'existe pas dans le fichier.", status_code=400)
+        
+        # Calculer le max pour la colonne spécifiée
+        mean_value = str(df[column].max())
+        result = {"max": mean_value}  # Remplacez avec la logique réelle
+        return func.HttpResponse(json.dumps(result), status_code=200, mimetype="application/json")
+
+    except ValueError:
+        return func.HttpResponse("Erreur : données JSON invalides.", status_code=400)
+    except Exception as e:
+        logging.error(f"Erreur : {str(e)}")
+        return func.HttpResponse(f"Erreur : {str(e)}", status_code=500)
 
 # anotherfunction
 @app.function_name("anotherfunction")
